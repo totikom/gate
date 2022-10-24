906c5aab6eaf93845eda8352b02681a5cab824d5
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-10-24 11:17:35 +0300

    Added two qubit gates

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,13 @@ use num_complex::{Complex, Complex64};
 type Amp = Complex<f32>;
 
 #[derive(Debug, Clone, PartialEq)]
-struct State(Vec<Amp>);
+pub struct State(Vec<Amp>);
 
 #[derive(Debug, Clone, PartialEq)]
-struct SingleQubitGate([[Amp; 2]; 2]);
+pub struct SingleQubitGate([[Amp; 2]; 2]);
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TwoQubitGate([[Amp; 4]; 4]);
 
 impl State {
     pub fn apply_single_qubit_gate(&self, index: usize, gate: &SingleQubitGate) -> Self {
@@ -23,127 +26,311 @@ impl State {
         }
         Self(result)
     }
+
+    pub fn apply_two_qubit_gate(
+        &self,
+        first_index: usize,
+        second_index: usize,
+        gate: &TwoQubitGate,
+    ) -> Self {
+        let mut result: Vec<Amp> = vec![Complex::new(0.0, 0.0); self.0.len()];
+
+        for i in 0..self.0.len() {
+            dbg!(i);
+            let gate_index = if second_index == 0 {
+                (i & 1 << first_index) >> first_index | (i & 1 << second_index) << 1
+            } else {
+                (i & 1 << first_index) >> first_index | (i & 1 << second_index) >> (second_index - 1)
+                   
+            };
+            dbg!(gate_index);
+
+            result[i] = gate.0[gate_index][0] * self.0[(i & !(1 << first_index)) & !(1 << second_index)]
+                      + gate.0[gate_index][1] * self.0[(i |  (1 << first_index)) & !(1 << second_index)]
+                      + gate.0[gate_index][2] * self.0[(i & !(1 << first_index)) |  (1 << second_index)]
+                      + gate.0[gate_index][3] * self.0[(i |  (1 << first_index)) |  (1 << second_index)];
+        }
+        Self(result)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::assert_eq;
 
-    #[test]
-    fn single_qubit() {
-        let qubit = State(vec![Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)]);
+    mod single_qubit_gate {
+        use super::*;
+        use pretty_assertions::assert_eq;
 
-        let matrix = SingleQubitGate([
-            [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
-            [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
-        ]);
+        #[test]
+        fn single_qubit() {
+            let matrix = SingleQubitGate([
+                [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
+                [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
+            ]);
 
-        let result = qubit.apply_single_qubit_gate(0, &matrix);
+            let qubit = State(vec![Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)]);
 
-        let expected_qubit = State(vec![Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)]);
+            let result = qubit.apply_single_qubit_gate(0, &matrix);
 
-        assert_eq!(result, expected_qubit);
+            let expected_qubit = State(vec![Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)]);
+
+            assert_eq!(result, expected_qubit);
+        }
+
+        #[test]
+        fn two_qubits() {
+            let matrix = SingleQubitGate([
+                [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
+                [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
+            ]);
+
+            let state = State(vec![
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            let result = state.apply_single_qubit_gate(1, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+
+            let result = state.apply_single_qubit_gate(0, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+        }
+
+        #[test]
+        fn three_qubits() {
+            let matrix = SingleQubitGate([
+                [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
+                [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
+            ]);
+
+            let state = State(vec![
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            let result = state.apply_single_qubit_gate(0, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+
+            let result = state.apply_single_qubit_gate(1, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+
+            let result = state.apply_single_qubit_gate(2, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+        }
     }
 
-    #[test]
-    fn two_qubits() {
-        let state = State(vec![
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+    mod two_qubit_gate {
+        use super::*;
+        use pretty_assertions::assert_eq;
 
-        let matrix = SingleQubitGate([
-            [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
-            [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
-        ]);
+        #[test]
+        fn two_qubits() {
+            let cnot = TwoQubitGate([
+                [
+                    Complex::new(1.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                ],
+                [
+                    Complex::new(0.0, 0.0),
+                    Complex::new(1.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                ],
+                [
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(1.0, 0.0),
+                ],
+                [
+                    Complex::new(0.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                    Complex::new(1.0, 0.0),
+                    Complex::new(0.0, 0.0),
+                ],
+            ]);
 
-        let result = state.apply_single_qubit_gate(1, &matrix);
+            let state = State(vec![
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        let expected_state = State(vec![
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+            let result = state.apply_two_qubit_gate(0, 1, &cnot);
 
-        assert_eq!(result, expected_state);
+            let expected_state = State(vec![
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        let result = state.apply_single_qubit_gate(0, &matrix);
+            assert_eq!(result, expected_state);
 
-        let expected_state = State(vec![
-            Complex::new(0.0, 0.0),
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+            let state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        assert_eq!(result, expected_state);
-    }
+            let result = state.apply_two_qubit_gate(0, 1, &cnot);
 
-    #[test]
-    fn three_qubits() {
-        let state = State(vec![
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        let matrix = SingleQubitGate([
-            [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
-            [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
-        ]);
+            assert_eq!(result, expected_state);
 
-        let result = state.apply_single_qubit_gate(0, &matrix);
+            let state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        let expected_state = State(vec![
-            Complex::new(0.0, 0.0),
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+            let result = state.apply_two_qubit_gate(0, 1, &cnot);
 
-        assert_eq!(result, expected_state);
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+            ]);
 
-        let result = state.apply_single_qubit_gate(1, &matrix);
+            assert_eq!(result, expected_state);
+        }
 
-        let expected_state = State(vec![
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+        #[test]
+        #[ignore]
+        fn three_qubits() {
+            let state = State(vec![
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        assert_eq!(result, expected_state);
+            let matrix = SingleQubitGate([
+                [Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)],
+                [Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)],
+            ]);
 
-        let result = state.apply_single_qubit_gate(2, &matrix);
+            let result = state.apply_single_qubit_gate(0, &matrix);
 
-        let expected_state = State(vec![
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(1.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-            Complex::new(0.0, 0.0),
-        ]);
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
 
-        assert_eq!(result, expected_state);
+            assert_eq!(result, expected_state);
+
+            let result = state.apply_single_qubit_gate(1, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+
+            let result = state.apply_single_qubit_gate(2, &matrix);
+
+            let expected_state = State(vec![
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ]);
+
+            assert_eq!(result, expected_state);
+        }
     }
 }
